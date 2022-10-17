b85b9b462e6b9b1a1fc81dd8324df96c5fc98fef
Arjun Vinod <arjun.vinod0014@gmail.com>
2022-10-17 18:25:10 +0530

    data binding

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,26 @@
+import { Comment } from './comments';
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments!: Comment[];
+
+    constructor(){
+        this.id = "";
+        this.name = "";
+        this.image = "";
+        this.category = "";
+        this.featured = true;
+        this.label = "";
+        this.price = "";
+        this.description = "";
+        this.comments = [];
+        
+
+    }
+}
