abde7e4af8d8a693db92d668e47312fb4adc55da
Arjun Vinod <arjun.vinod0014@gmail.com>
2022-10-25 16:12:47 +0530

    Rxjs part 1

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { Observable, of, lastValueFrom } from 'rxjs';
+import { delay } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,21 +11,22 @@ export class PromotionService {
   constructor() { }
 
   
-  getDishes(): Promise<Promotion[]>{
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS),3000); 
-  })
+  getDishes(): Observable<Promotion[]>{
+    return of(PROMOTIONS).pipe(delay(2000));
+     
+}
+// below converting observables to promises using lastvaluefrom instaed of toPromise() which is depricated
+//   getDish(id: string): Promise<Promotion> {
+//     return lastValueFrom(of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000)));
+    
+// }
+
+getDish(id: string): Observable<Promotion> {
+  return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+  
 }
 
-  getDish(id: string): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]),3000);
-  })
-}
-
-  getFeaturedDish(): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]),3000);
-  })
+  getFeaturedDish(): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promo) => promo.featured)[0]).pipe(delay(2000));
 }
 }
